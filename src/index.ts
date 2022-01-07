e1cc6d7da552653ce5d06835b2e5d28d61d88bee
franck-romano <franck.romano@protonmail.com>
2022-01-07 14:06:52 +0100

    feat(bus): expose bus through index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export const helloWorld = 'hello world';
+import { InternalEventBus } from './eventBus/InternalEventBus';
+import { InternalCommandBus } from './commandBus/InternalCommandBus';
+import { InternalQueryBus } from './queryBus/InternalQueryBus';
+
+export const EventBus = new InternalEventBus();
+export const QueryBus = new InternalCommandBus();
+export const CommandBus = new InternalQueryBus();
