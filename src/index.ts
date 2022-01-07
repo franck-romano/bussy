b8e6504041bc6b8802b30244e55e64cfeeb0cf7a
franck-romano <franck.romano@protonmail.com>
2022-01-07 17:46:53 +0100

    feat(index): expose class constructor
    This might change but for now it will do the job

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import { InternalEventBus } from './eventBus/InternalEventBus';
 import { InternalCommandBus } from './commandBus/InternalCommandBus';
 import { InternalQueryBus } from './queryBus/InternalQueryBus';
-import { LoggingEventBusMiddleware } from './eventBus/middlewares/LoggingEventBusMiddleware';
-import { PinoLogger } from './common/BusLogger';
-import { LoggingQueryBusMiddleware } from './queryBus/middlewares/LoggingQueryBusMiddleware';
-import { QueryBusDispatcherMiddleware } from './queryBus/middlewares/QueryBusDispatcherMiddleware';
 
-const logger = new PinoLogger();
-
-export const EventBus = new InternalEventBus(logger).registerMiddlewares([new LoggingEventBusMiddleware(logger)]);
-export const QueryBus = new InternalCommandBus();
-const middleWares = new LoggingQueryBusMiddleware(new QueryBusDispatcherMiddleware({}), logger);
-
-export const CommandBus = new InternalQueryBus(middleWares);
+export const EventBus = InternalEventBus;
+export const CommandBus = InternalCommandBus;
+export const QueryBus = InternalQueryBus;
