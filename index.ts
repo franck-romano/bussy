8bde31248eb501c03c5af991000abb25a8fa1c04
franck-romano <franck.romano@protonmail.com>
2022-08-19 10:53:24 +0200

    feat: export basic query and command dispatcher middlewares

--- index.ts
+++ index.ts
@@ -9,11 +9,15 @@ export const QueryBus = InternalQueryBus;
 export { Command } from './src/commandBus/types/Command';
 export { VoidCommand } from './src/commandBus/types/VoidCommand';
 export { CommandHandler } from './src/commandBus/types/CommandHandler';
+export { CommandMiddleware } from './src/commandBus/middlewares/CommandMiddleware';
 export { CommandResponse } from './src/commandBus/types/CommandResponse';
+export { CommandBusDispatcherMiddleware } from './src/commandBus/middlewares/CommandBusDispatcherMiddleware';
 
 export { Query } from './src/queryBus/types/Query';
 export { ReadModel } from './src/queryBus/types/ReadModel';
 export { QueryHandler } from './src/queryBus/types/QueryHandler';
+export { QueryMiddleware } from './src/queryBus/middlewares/QueryMiddleware';
+export { QueryBusDispatcherMiddleware } from './src/queryBus/middlewares/QueryBusDispatcherMiddleware';
 
 export { DomainEvent } from './src/eventBus/types/DomainEvent';
 export { EventHandler } from './src/eventBus/types/EventHandler';
